e0f4eec20e1e2bb07a1813a1edd019933edeceaa
felipecsl <felipe.lima@gmail.com>
2012-06-24 20:09:04 -0700

    Fix bug on malformed selectors
    Whenever a property selector does not match a string starting
    with xpath= or css=, it is gonna ignore it and return itself in
    the resulting hash.

--- spec/wombat_spec.rb
+++ spec/wombat_spec.rb
@@ -4,4 +4,19 @@ describe Wombat do
 	it 'should provide syntactic sugar method Wombat.crawl' do
 		Wombat.should respond_to(:crawl)
 	end
+
+	it 'should accept regular properties (non-selectors)' do
+		VCR.use_cassette('broken_selector') do
+			lambda { 
+				Wombat.crawl do
+					base_url "http://www.github.com"
+		  		list_page "/"
+
+		  		source :obaoba
+			    description 'Oba Oba'
+			    website 'http://obaoba.com.br'
+				end
+			}.should_not raise_error
+		end
+	end
 end
