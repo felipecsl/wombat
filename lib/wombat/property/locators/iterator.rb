c3343fab24e0a93ccbcae8a8a00ce5d0d67708a6
Oscar Rendon <oarendon@gmail.com>
2012-12-20 08:56:18 -0500

    Refactor to avoid duplitacion #codeclimate

--- lib/wombat/property/locators/iterator.rb
+++ lib/wombat/property/locators/iterator.rb
@@ -8,12 +8,7 @@ module Wombat
       	def locate(contex, page = nil)
           super do
             locate_nodes(contex).flat_map do |node|
-              Hash.new.tap do |h|
-                @property.values
-                  .select { |v| v.is_a?(Wombat::DSL::Property) || v.is_a?(Wombat::DSL::PropertyGroup) }
-                  .map { |p| Factory.locator_for(p).locate(node, page) }
-                  .map { |p| h.merge! p }
-              end
+              filter_properties(node, page)
             end
           end
         end
